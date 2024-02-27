b0808a5f32c8f8b013162d8e8365bd3fc47c332c
Kirill Sergeyuk <kirillsergeuk86@gmail.com>
2024-02-27 22:08:10 +0300

    Creat 4.0 version of project

--- src/QE/FormattingEquation.java
+++ src/QE/FormattingEquation.java
@@ -3,39 +3,47 @@ package QE;
 import java.util.Objects;
 
 public class FormattingEquation {
+    private boolean isEqualTo = true;
     private String[] odds;
     private int a = 0,b = 0,c = 0;
     public void format(String equation){
-        equation = equation.replace(" = 0", "");
         equation = equation.replace("+ ", "");
-        equation = equation.replace("- ", "-1");
-        equation = equation.replace("-", "-1");
+        equation = equation.replace("- ", "-");
+
         System.out.println(equation);
         odds =  equation.split(" ");
-        System.out.println(odds.length);
         for(int i = 0; i < odds.length; i++){
+            if(odds[i].equals("=")){
+                isEqualTo = false;
+                continue;
+            }
             if(odds[i].contains("x2")){
                 odds[i] = odds[i].replace("x2", "");
-                if(odds[i].isEmpty()){
-                    setA(1);
-                }   else{
-                    setA(Integer.parseInt(odds[i]));
+                if(odds[i].isEmpty() || odds[i].equals("-")){
+                    odds[i] += "1";
                 }
+                if (isEqualTo) a += Integer.parseInt(odds[i]);
+                else a -= Integer.parseInt(odds[i]);
                 continue;
             }
             if(odds[i].contains("x")){
                 odds[i] = odds[i].replace("x", "");
-                if(odds[i].isEmpty()){
-                    setB(1);
-                }   else{
-                    setB(Integer.parseInt(odds[i]));
+                if(odds[i].isEmpty() || odds[i].equals("-")){
+                    odds[i] += "1";
                 }
+                if (isEqualTo) b += Integer.parseInt(odds[i]);
+                else b -= Integer.parseInt(odds[i]);
                 continue;
             }
             if((!odds[i].contains("x")) && (!odds[i].contains("x2"))){
-                setC(Integer.parseInt(odds[i]));
+                if(isEqualTo) c += Integer.parseInt(odds[i]);
+                else c -= Integer.parseInt(odds[i]);
             }
         }
+        System.out.println(a);
+        setA(a);
+        setB(b);
+        setC(c);
     }
     public void setA(int a) {
         this.a = a;
